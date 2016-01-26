openstack/neutron

Wrap ML2 delete_port with db retry decorator
ML2 delete_port operation currently involves locking ports
and bindings tables which may lead to DBDeadlock errors in certain
cases when several ports are deleted concurrently.
That may happen due to specifics of Galera working in active-active
mode: it may throw deadlock errors when it fails to validate
a change with other members of the cluster.
The fix adds retries to delete port operation to overcome such
deadlocks

Closes-Bug: #1422504
Change-Id: I684691d59c5ac370d74314c3c91857dc709b2d9b

neutron/plugins/ml2/plugin.py
@@ -1235,6 +1235,8 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
                 raise e.errors[0].error
             raise exc.ServicePortInUse(port_id=port_id, reason=e)
 
+    @oslo_db_api.wrap_db_retry(max_retries=db_api.MAX_RETRIES,
+                               retry_on_deadlock=True)
     def delete_port(self, context, id, l3_port_check=True):
         self._pre_delete_port(context, id, l3_port_check)
         # TODO(armax): get rid of the l3 dependency in the with block

venkataanil/juno_neutron

Conflicts:
    neutron/plugins/ml2/plugin.py
    neutron/tests/unit/ml2/test_ml2_plugin.py
Upstream-Juno: https://review.openstack.org/#/c/231985/1
Closes rhbz: 1268787
(cherry picked from commit 45ea2cf10033e12c63b8ce2cd78b04755d0aba64)
(cherry picked from commit 151b28cf1ce697f481d65b8d5bee1e3a4becbbc5)
(cherry picked from commit f0bb982bfc81bb7c5bafde1430c51bea8f8ab9c9)
Reviewed-on: https://code.engineering.redhat.com/gerrit/58955
Tested-by: RHOS Jenkins <apevec+rhosci@redhat.com>
Reviewed-by: Ihar Hrachyshka <ihrachys@redhat.com>

neutron/plugins/ml2/plugin.py
@@ -986,6 +986,8 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
                 cur_binding.driver = new_binding.driver
                 cur_binding.segment = new_binding.segment
 
+    @db_api.wrap_db_retry(max_retries=db_api.MAX_RETRIES,
+                          retry_on_deadlock=True)
     def delete_port(self, context, id, l3_port_check=True):
         LOG.debug(_("Deleting port %s"), id)
         removed_routers = []