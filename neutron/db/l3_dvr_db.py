wolverineav/neutron

L3 DB: Defer port DB subnet lookups
_populate_subnets_for_ports was being called multiple
times for different interface types during the get_routers
process.

This patch eliminates those extra queries by deferring the
subnet information population until after all of the interfaces
have been looked up.

Conflicts:
	neutron/db/l3_db.py
	neutron/db/l3_dvr_db.py

Change-Id: Ib46f685d72eb61ecbaa2869e28fb173cd6d49552
Partial-bug: #1445412
(cherry picked from commit c2f24dc8e3c3bcc48e19793e2ced438bd3e326ff)

neutron/db/l3_dvr_db.py
@@ -275,8 +275,6 @@ class L3_NAT_with_dvr_db_mixin(l3_db.L3_NAT_db_mixin,
         interfaces = [self._core_plugin._make_port_dict(rp.port, None)
                       for rp in qry]
         LOG.debug("Return the SNAT ports: %s", interfaces)
-        if interfaces:
-            self._populate_subnet_for_ports(context, interfaces)
         return interfaces
 
     def _build_routers_list(self, context, routers, gw_ports):
@@ -351,8 +349,6 @@ class L3_NAT_with_dvr_db_mixin(l3_db.L3_NAT_db_mixin,
                    'device_owner': [DEVICE_OWNER_AGENT_GW]}
         interfaces = self._core_plugin.get_ports(context.elevated(), filters)
         LOG.debug("Return the FIP ports: %s ", interfaces)
-        if interfaces:
-            self._populate_subnet_for_ports(context, interfaces)
         return interfaces
 
     def get_sync_data(self, context, router_ids=None, active=None):
@@ -365,6 +361,14 @@ class L3_NAT_with_dvr_db_mixin(l3_db.L3_NAT_db_mixin,
             fip['host'] = self.get_vm_port_hostid(context, fip['port_id'])
         routers_dict = self._process_routers(context, routers)
         self._process_floating_ips(context, routers_dict, floating_ips)
+        ports_to_populate = []
+        for router in routers_dict.values():
+            if router.get('gw_port'):
+                ports_to_populate.append(router['gw_port'])
+            if router.get(l3_const.FLOATINGIP_AGENT_INTF_KEY):
+                ports_to_populate += router[l3_const.FLOATINGIP_AGENT_INTF_KEY]
+        ports_to_populate += interfaces
+        self._populate_subnets_for_ports(context, ports_to_populate)
         self._process_interfaces(routers_dict, interfaces)
         return routers_dict.values()
 

venkataanil/juno_neutron

have been looked up. Includes a function rename as well to
indicate that a function is only used internally.
Renamed _populate_subnets_for_ports to _populate_subnet_for_ports in
patch to match current version.
Upstream-Liberty: https://review.openstack.org/#/c/174843/
Related rhbz: 1281583
(cherry picked from commit 9701bd479529ccc243e48fdb944c284d2921c376)
Reviewed-on: https://code.engineering.redhat.com/gerrit/61596
Reviewed-by: Miguel Angel Ajo Pelayo <majopela@redhat.com>
Tested-by: RHOS Jenkins <apevec+rhosci@redhat.com>

neutron/db/l3_dvr_db.py
@@ -275,8 +275,6 @@ class L3_NAT_with_dvr_db_mixin(l3_db.L3_NAT_db_mixin,
         interfaces = [self._core_plugin._make_port_dict(rp.port, None)
                       for rp in qry]
         LOG.debug("Return the SNAT ports: %s", interfaces)
-        if interfaces:
-            self._populate_subnet_for_ports(context, interfaces)
         return interfaces
 
     def _build_routers_list(self, context, routers, gw_ports):
@@ -347,8 +345,6 @@ class L3_NAT_with_dvr_db_mixin(l3_db.L3_NAT_db_mixin,
                    'device_owner': [DEVICE_OWNER_AGENT_GW]}
         interfaces = self._core_plugin.get_ports(context.elevated(), filters)
         LOG.debug("Return the FIP ports: %s ", interfaces)
-        if interfaces:
-            self._populate_subnet_for_ports(context, interfaces)
         return interfaces
 
     def get_dvr_sync_data(self, context, host, agent, router_ids=None,
@@ -369,6 +365,14 @@ class L3_NAT_with_dvr_db_mixin(l3_db.L3_NAT_db_mixin,
         routers_dict = self._process_routers(context, routers)
         self._process_floating_ips_dvr(context, routers_dict,
                                        floating_ips, host, agent)
+        ports_to_populate = []
+        for router in routers_dict.values():
+            if router.get('gw_port'):
+                ports_to_populate.append(router['gw_port'])
+            if router.get(l3_const.FLOATINGIP_AGENT_INTF_KEY):
+                ports_to_populate += router[l3_const.FLOATINGIP_AGENT_INTF_KEY]
+        ports_to_populate += interfaces
+        self._populate_subnet_for_ports(context, ports_to_populate)
         self._process_interfaces(routers_dict, interfaces)
         return routers_dict.values()
 